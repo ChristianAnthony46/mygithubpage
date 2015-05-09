173e434f942e6b10824b77985eb49569911b6de4
Swordslam <christian_grindell@falconmail.daytonastate.edu>
2015-05-09 14:12:01 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>
+            Blobfish 2016
+        </title>
+    </head>
+
+    <body>
+        <img src="http://www.nationofchange.org/2015/wp-content/uploads/TedCruz031415.jpg" height="50%" width="50%" />
+        <img src="https://critter.science/wp-content/uploads/2015/04/POTD_Blobfish_2930969k.jpg" height="50%" width="50%" />
+    </body>
+
+</html>
